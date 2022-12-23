7bc2dfac9956fc4b4e887ebedca023c6752cbf45
Sumit.Kumar <sumit.kumar@qtsolv.com>
2022-12-23 10:23:41 +0530

    working fine

---
+++ pages/events/[id].js
@@ -0,0 +1,7 @@
+const Page=()=>{
+    return (
+        <div>Our Single Events</div>
+    )
+}
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,24 @@
+const Page=()=>{
+    return(
+        <div>
+            <h1>Event Page</h1>
+           <a href="/">
+          <img/>
+          <h2>Events in London</h2>
+         
+        </a>
+        <a href="/">
+          <img/>
+          <h2>Events in Sans Franscisco</h2>
+         
+        </a>
+        <a href="/">
+          <img/>
+          <h2>Events in Sans Barcilona</h2>
+         
+        </a>
+        </div>
+    )
+}
+
+export default Page;
